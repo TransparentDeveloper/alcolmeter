d4de0f65c690466fe56c23307787eb20ac4fde96
TransparentDeveloper <2younsin@gmail.com>
2026-05-09 08:52:59 +0900

    feat: Chrome 확장프로그램 프로젝트 스캐폴드 추가 (WXT, vanilla-ts)

---
+++ apps/extension/entrypoints/popup/main.ts
@@ -0,0 +1,4 @@
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h1>Alcolmeter</h1>
+  <p>막걸리 알코올 도수 계산기</p>
+`;
---
+++ apps/extension/wxt.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'wxt';
+
+export default defineConfig({
+	manifest: {
+		name: 'Alcolmeter',
+		description: '막걸리 알코올 도수 계산기',
+		version: '0.1.0'
+	}
+});
