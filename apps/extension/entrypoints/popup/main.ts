7d649133794bda230c9ee61546befbadfcd14635
TransparentDeveloper <2younsin@gmail.com>
2026-05-10 22:07:52 +0900

    chore: extension 팝업 진입점을 Svelte 마운트 방식으로 전환

--- apps/extension/entrypoints/popup/main.ts
+++ apps/extension/entrypoints/popup/main.ts
@@ -1,4 +1,4 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1>Alcolmeter</h1>
-  <p>막걸리 알코올 도수 계산기</p>
-`;
+import { mount } from 'svelte';
+import App from './App.svelte';
+
+mount(App, { target: document.getElementById('app')! });
